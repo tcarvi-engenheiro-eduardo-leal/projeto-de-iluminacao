aedb7b7f70368d586f4c79dff4783ec8fa1144ec
tcarvi <tcarvi@me.com>
2024-07-15 05:23:20 -0300

    Update

--- tutorial/docusaurus.config.ts
+++ tutorial/docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/tcarvi-engenheiro-eduardo-leal/projeto-de-iluminacao/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/tcarvi-engenheiro-eduardo-leal/projeto-de-iluminacao/edit/main/tutorial/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/tcarvi-engenheiro-eduardo-leal/projeto-de-iluminacao/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/tcarvi-engenheiro-eduardo-leal/projeto-de-iluminacao/edit/main/tutorial/',
         },
         theme: {
           customCss: './src/css/custom.css',
